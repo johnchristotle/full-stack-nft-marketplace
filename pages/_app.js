df89662448b8b2e40cc3d94cda6ae35cfdd35811
Christotle Agholor <96313953+johnchristotle@users.noreply.github.com>
2022-02-16 16:19:17 -0800

    Update _app.js
    changed linked text colours

--- pages/_app.js
+++ pages/_app.js
@@ -8,22 +8,22 @@ function Marketplace({ Component, pageProps }) {
         <p className="text-4xl font-bold">De-Christotle NFT Marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
+            <a className="mr-4 text-blue-500">
               Market
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-blue-500">
               Sell NFTs
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-blue-500">
               My NFTs
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-blue-500">
               Dashboard
             </a>
           </Link>
