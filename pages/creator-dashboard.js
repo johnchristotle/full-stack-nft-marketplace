3cdc6e52d2ed8a51ef3257fe75dfb2b85654f768
Christotle Agholor <96313953+johnchristotle@users.noreply.github.com>
2022-02-16 16:50:15 -0800

    Update creator-dashboard.js

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -50,18 +50,18 @@ export default function CreatorDashboard() {
     setNfts(items)
     setLoadingState('loaded') 
   }
-  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets created</h1>)
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No NFT Listed</h1>)
   return (
     <div>
       <div className="p-4">
-        <h2 className="text-2xl py-2">Items Created</h2>
+        <h2 className="text-2xl py-2">NFT Listed</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {
             nfts.map((nft, i) => (
               <div key={i} className="border shadow rounded-xl overflow-hidden">
                 <img src={nft.image} className="rounded" />
                 <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                  <p className="text-2xl font-bold text-white">Price - {nft.price} Matic</p>
                 </div>
               </div>
             ))
@@ -79,7 +79,7 @@ export default function CreatorDashboard() {
                     <div key={i} className="border shadow rounded-xl overflow-hidden">
                       <img src={nft.image} className="rounded" />
                       <div className="p-4 bg-black">
-                        <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                        <p className="text-2xl font-bold text-white">Price - {nft.price} Matic</p>
                       </div>
                     </div>
                   ))
